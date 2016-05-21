d6d10a6ae3fdb17b48e2fc1ef9a0f39080295a6e
Bob Weichler <bobmasaki@hotmail.com>
2016-05-21 12:22:14 +0200

    Question table body toegevoegd

--- models/Questions.js
+++ models/Questions.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 var QuestionSchema = new mongoose.Schema({
 	title: String,
+	body: String,
 	link: String,
 	upvotes: {type: Number, default: 0},
 	comments: [{type:mongoose.Schema.Types.ObjectId, ref:'Answer'}]
