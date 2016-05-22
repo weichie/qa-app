ec1db39557d3dfdc62c92fc9c0b3918a5ddc3cae
Bob Weichler <bobmasaki@hotmail.com>
2016-05-22 12:34:38 +0200

    latest push (minor updates)

--- public/javascripts/qa-app.js
+++ public/javascripts/qa-app.js
@@ -241,6 +241,10 @@ app.controller('NavCtrl', ['$scope', 'auth', function($scope, auth){
 	$scope.isLoggedIn = auth.isLoggedIn;
 	$scope.currentUser = auth.currentUser;
 	$scope.logOut = auth.logOut;
+
+	$scope.logOut = function(){
+		auth.logout();
+	};
 }]);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
